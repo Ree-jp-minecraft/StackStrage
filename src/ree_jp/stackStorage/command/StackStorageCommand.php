6a615f41f46bc5bb021cd5303324da6e71c46e2b
Ree_ <mail@ree-jp.net>
2020-08-07 22:16:42 +0900

    fix #7 update: version

--- src/ree_jp/stackStorage/command/StackStorageCommand.php
+++ src/ree_jp/stackStorage/command/StackStorageCommand.php
@@ -6,17 +6,21 @@ namespace ree_jp\stackStorage\command;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\command\PluginIdentifiableCommand;
 use pocketmine\Player;
+use pocketmine\plugin\Plugin;
 use pocketmine\utils\TextFormat;
 use ree_jp\stackStorage\api\StackStorageAPI;
 
-class StackStorageCommand extends Command
+class StackStorageCommand extends PluginCommand
 {
-	public function __construct()
+	public function __construct(Plugin $owner)
 	{
-		parent::__construct('stackstorage', 'StackStorage Command', '/stackstorage', ['st']);
+		parent::__construct('stackstorage', $owner);
 		$this->setPermission("stackstorage.command");
 		$this->setPermissionMessage('§cSet permissions from \'plugin.yml\' to \'true\' to allow use without permissions');
+		$this->setAliases(["st"]);
 	}
 
 	/**
@@ -25,7 +29,7 @@ class StackStorageCommand extends Command
 	public function execute(CommandSender $sender, string $commandLabel, array $args)
 	{
 		if (!$sender instanceof Player) {
-			$sender->sendMessage(TextFormat::RED . '>> '. TextFormat::RESET. 'StackStorageCommand error');
+			$sender->sendMessage(TextFormat::RED . '>> ' . TextFormat::RESET . 'StackStorageCommand error');
 			return;
 		}
 		if (!$this->testPermission($sender)) return;
