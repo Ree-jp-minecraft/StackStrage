9423abb72723c4d8f98dfff542817fa46c04e94f
Kondo Ryosuke <mail@ree-jp.net>
2021-12-04 00:52:34 +0900

    sqliteとmysqlで処理の仕方を変える

--- src/ree_jp/stackStorage/sql/StackStorageHelper.php
+++ src/ree_jp/stackStorage/sql/StackStorageHelper.php
@@ -15,6 +15,7 @@ class StackStorageHelper implements IStackStorageHelper
 {
     static StackStorageHelper $instance;
 
+    private string $type;
     private DataConnector $db;
 
     /**
@@ -23,6 +24,7 @@ class StackStorageHelper implements IStackStorageHelper
     public function __construct(PluginBase $plugin, string $path)
     {
         $config = new Config($path . 'sql.yml');
+        $this->type = $config->get("database")["type"];
         $this->db = libasynql::create($plugin, $config->get('database'), [
             'mysql' => 'mysql.sql',
             'sqlite' => 'sqlite.sql'
@@ -55,8 +57,28 @@ class StackStorageHelper implements IStackStorageHelper
     public function addItem(string $xuid, Item $item, ?Closure $func, ?Closure $failure): void
     {
         $jsonItem = json_encode((clone $item)->setCount(0));
-        $this->db->executeGeneric('StackStorage.add', ["xuid" => $xuid, "item" => $jsonItem, "count" => $item->getCount()], $func, $failure);
+        if ($this->type === "mysql") {
+            $this->db->executeGeneric('StackStorage.add', ["xuid" => $xuid, "item" => $jsonItem, "count" => $item->getCount()], $func, $failure);
+        } elseif ($this->type === "sqlite") {
+            $this->getItem($xuid, $item, function (array $rows) use ($item, $failure, $func, $jsonItem, $xuid): void {
+                $arrayItem = array_shift($rows);
+
+                if (isset($arrayItem['count'])) {
+                    $count = $arrayItem['count'] + $item->getCount();
+                } else {
+                    $this->db->executeInsert('StackStorage.set', ['xuid' => $xuid, 'item' => $jsonItem, 'count' => $item->getCount()], $func, $failure);
+                    return;
+                }
+
+                if ($count > 0) {
+                    $this->db->executeInsert('StackStorage.update', ['xuid' => $xuid, 'item' => $jsonItem, 'count' => $count], $func, $failure);
+                } else {
+                    $this->db->executeGeneric('StackStorage.delete', ['xuid' => $xuid, 'item' => $jsonItem], $func, $failure);
+                }
+            }, $failure);
+        }
     }
+
 
     /**
      * @inheritDoc
